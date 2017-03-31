bdaaa787d2d291592609ba35b48b64610cb8b962
Randy <randypriv@gmail.com>
2017-03-31 15:51:38 +0800

    fetch posts

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import React from 'react'
 import { BrowserRouter } from 'cans/router'
 import App from './App'
 
+// cans plugins
+import { restPlugin } from 'cans-plugin-http'
+
 const app = cans()
+
+app.use(restPlugin, {
+  resources: [
+    { name: 'posts', url: 'http://jsonplaceholder.typicode.com' }
+  ]
+})
 
 app.route(() => (
   <BrowserRouter>
--- src/views/Curd.js
+++ src/views/Curd.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import { inject } from 'cans'
+import {
+  Table,
+  Button
+} from 'antd'
+
+const DataTable = inject(({ models }) => {
+  const columns = [
+    {
+      title: 'id',
+      dataIndex: 'id'
+    },
+    {
+      title: 'title',
+      dataIndex: 'title'
+    },
+    {
+      title: 'body',
+      dataIndex: 'body'
+    }
+  ]
+
+  return (
+    <div>
+      <Button loading={models.rest.posts.loading.index} onClick={models.rest.posts.index}>Fetch Posts</Button>
+      <Table
+        rowKey='id'
+        loading={models.rest.posts.loading.index}
+        columns={columns}
+        dataSource={models.rest.posts.data.index}
+      />
+    </div>
+  )
+})
 
 const Curd = () => {
   return (
     <div>
-      CURD
+      <DataTable />
     </div>
   )
 }
